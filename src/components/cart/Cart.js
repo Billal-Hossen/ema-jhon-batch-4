4424ce4c2db091afe134b9b9894e4579a5b2edd1
Billal-Hossen <76785472+Billal-Hossen@users.noreply.github.com>
2022-01-29 16:53:32 +0600

    Cart issues fixed

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -8,12 +8,19 @@ const Cart = (props) => {
 
     // }
     const cartReducer=(previous,current)=>previous+current.price;
-    const totalPrice=cart.reduce(cartReducer,0);
+    const totalPrice=(cart.reduce(cartReducer,0));
+    const delivayCharge=15;
+    const tax= ((totalPrice+delivayCharge)*.10);
+    const grandTotal=(totalPrice+delivayCharge+tax);
     return (
         <div style={{"textAlign":'center'}}>
         <h3>Order Summary</h3>
         <h5>Order Quantity:{cart.length}</h5>
-        <h6>Total: {totalPrice} </h6>
+        <h6>Total: {totalPrice.toFixed(2)} </h6>
+        <p>DelevaryChage: {cart.length ?  delivayCharge : " 0"}</p>
+        <p>Tax: {cart.length ? tax.toFixed(2) : " 0"}</p>
+        <h5>Grand Total:{cart.length ? grandTotal.toFixed(2) : " 0" } </h5>
+
         
     </div>
     );
